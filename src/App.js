9c9b789a5c020898ff53685d1d0e6b4af5893e37
eunjin0212 <eunjin9639@gmail.com>
2020-10-22 15:38:21 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <sapn>something</sapn>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/About" component={About} />
+    </HashRouter>
+  );
 }
 export default App;
